e959a23e2c7843031e315d0d8a0e67f2e2349998
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-20 18:05:40 -0300

    Feat: Create config

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    define("HOST", "localhost");
+    define("USER", "root");
+    define("PASSWORD", "12709Everton$");
+    define("BASE", "agenda");
+
+    $conn = new MySQLi(HOST, USER, PASSWORD, BASE);
+    
