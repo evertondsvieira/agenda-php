19cde75d78519c72c0e36a99a7b067ed7f25def0
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-21 00:34:53 -0300

    Fix: fixed index

--- index.php
+++ index.php
@@ -1,21 +1,50 @@
+<?php
+    session_start();
+    if (isset($_SESSION['username'])) {
+        include("header.php");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
-    <title>Agenda</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Lexend:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>YourAgenda</title>
 </head>
 <body>
-    <header class="header">
-        <p>Agenda<p>
-        <ul class=header-ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="?page=add">Adicionar novo contato</a></li>
-            <li><a href="?page=list">Listar contato</a></li>
-        </ul>
-    </header>
+    <?php
+        if ($_SERVER['REQUEST_URI'] == '/') {
+            include("index.php");
+            include("home.php");
+        } else {
+            if (!isset($_SESSION['username'])) {
+                echo '
+                    <div class="centralize-form">
+                        <h1 class="login-name">Login</h1>
+                        <form action="login.php" method="POST">
+                            <div class="centralize-input">
+                                <label>Username</label>
+                                <input class="input" type="text" name="username">
+                            </div>
+                            <div class="centralize-input">
+                                <label>Senha</label>
+                                <input class="input" type="password" name="password"> 
+                            </div>
+                            <div class="centralize-button">
+                                <button class="button-submit" type="submit">Enviar</button>
+                            </div>
+                        </form>
+                    </div>
+                ';
+            }
+        }
+    ?>
 
     <div>
         <div>
@@ -35,15 +64,10 @@
                         case "save":
                             include("save-contact.php");
                         break;
-                        default:
-                            print "<h1>Bem-vindos!</h1>";
                     }
                 ?>
             </div>
         </div>
     </div>
-
-
-    <script src=""></script>
 </body>
 </html>
