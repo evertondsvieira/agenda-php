c45705006dabff6ba0d332fd6663cccbfe9501cc
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-21 00:33:36 -0300

    Fix: Fixed add-contact

--- add-contact.php
+++ add-contact.php
@@ -1,32 +1,25 @@
-<header class="header">
-    <p>Agenda<p>
-    <ul class=header-ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="?page=add">Adicionar novo contato</a></li>
-        <li><a href="?page=list">Listar contato</a></li>
-    </ul>
-</header>
-    
-<h1>Novo contato</h1>
-<form action="?page=save" method="POST">
-    <input type="hidden" name="action" value="create">
-    <div>
-        <label>Nome:</label>
-        <input type="text" name="name" required>
-    </div>
-    <div>
-        <label>Email:</label>
-        <input type="email" name="email" required>
-    </div>
-    <div>
-        <label>Telefone:</label>
-        <input type="text" name="phone" required>
-    </div>
-    <div>
-        <label>User id:</label>
-        <input type="text" name="user_id" required>
-    </div>
-    <div>
-        <button type="submit">Enviar</button>
-    </div>
-</form>
+<div class="centralize-form">
+    <h1 class="add-name">Novo contato</h1>
+    <form action="?page=save" method="POST">
+        <input type="hidden" name="action" value="create">
+        <div class="centralize-input">
+            <label>Nome:</label>
+            <input class="input" type="text" name="name" required>
+        </div>
+        <div class="centralize-input">
+            <label>Email:</label>
+            <input class="input" type="email" name="email" required>
+        </div>
+        <div class="centralize-input">
+            <label>Telefone:</label>
+            <input class="input" type="text" name="phone" required>
+        </div>
+        <div class="centralize-input">
+            <label>User id:</label>
+            <input class="input" type="text" name="user_id" required>
+        </div>
+        <div class="centralize-button">
+            <button class="button-submit" type="submit">Enviar</button>
+        </div>
+    </form>
+</div>
