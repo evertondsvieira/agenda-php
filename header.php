dabfa7da2f9abde68111741600f6663e5d771e10
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-21 00:34:10 -0300

    Feat: Create header

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Lexend:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="index.css">
+    <title>YourAgenda</title>
+</head>
+<body>
+    <header class="header">
+        <p class="logo">Agenda<p>
+        <ul class="header-ul">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="?page=add">Adicionar novo contato</a></li>
+            <li><a href="?page=list">Listar contato</a></li>
+            <li><a href='logout.php'>Sair</a></li>
+        </ul>
+    </header>
+</body>
