13a95777ad243a3fef694927b444effee96ce996
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-21 00:35:47 -0300

    Fix: Fixed contact

--- update-contact.php
+++ update-contact.php
@@ -1,28 +1,29 @@
-<h1>Editar contato</h1>
-
 <?php
     $sql = "SELECT * FROM contact WHERE id=".$_REQUEST["id"];
     $res = $conn->query($sql);
     $row = $res->fetch_object();
 ?>
 
-<form action="?page=save" method="POST">
-    <input type="hidden" name="action" value="update">
-    <input type="hidden" name="id" value="<?php print $row->id; ?>">
+<div class="centralize-form">
+    <h1 class="update-name">Editar contato</h1>
+    <form action="?page=save" method="POST">
+        <input type="hidden" name="action" value="update">
+        <input type="hidden" name="id" value="<?php print $row->id; ?>">
 
-    <div>
-        <label>Nome:</label>
-        <input type="text" name="name" value="<?php print $row->name; ?>">
-    </div>
-    <div>
-        <label>Email:</label>
-        <input type="email" name="email" value="<?php print $row->email; ?>">
-    </div>
-    <div>
-        <label>Telefone:</label>
-        <input type="text" name="phone" value="<?php print $row->phone; ?>">
-    </div>
-    <div>
-        <button type="submit">Enviar</button>
-    </div>
-</form>
+        <div class="centralize-input">
+            <label>Nome:</label>
+            <input class="input" type="text" name="name" value="<?php print $row->name; ?>">
+        </div>
+        <div class="centralize-input">
+            <label>Email:</label>
+            <input class="input" type="email" name="email" value="<?php print $row->email; ?>">
+        </div>
+        <div class="centralize-input">
+            <label>Telefone:</label>
+            <input class="input" type="text" name="phone" value="<?php print $row->phone; ?>">
+        </div>
+        <div class="centralize-button">
+            <button class="button-submit" type="submit">Enviar</button>
+        </div>
+    </form>
+</div>
