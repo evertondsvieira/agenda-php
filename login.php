1d48ef69b5f1fe2953bdcda63130b5a40f394de4
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-22 02:08:21 -0300

    Fix: fixed hash password

--- login.php
+++ login.php
@@ -1,27 +1,32 @@
 <?php
-    session_start();
-    if(empty($_POST) or (empty($_POST["username"]) or (empty($_POST["password"])))){
-        print "<script>location.href='index.php';</script>";
-    }
+session_start();
+if(empty($_POST) or (empty($_POST["username"]) or (empty($_POST["password"])))){
+    print "<script>location.href='index.php';</script>";
+}
 
-    include('config.php');
+include('config.php');
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-    $sql = "SELECT * FROM user WHERE username = '{$username}' AND password = '{$password}'";
+$sql = "SELECT * FROM user WHERE username = '{$username}'";
 
-    $res = $conn->query($sql) or die($conn->error);
+$res = $conn->query($sql) or die($conn->error);
 
-    $row = $res->fetch_object();
+$row = $res->fetch_object();
 
-    $qtd = $res-> num_rows;
+$qtd = $res->num_rows;
 
-    if($qtd > 0){
+if($qtd > 0){
+    if(password_verify($password, $row->password)){
         $_SESSION['username'] = $username;
         print "<script>location.href='dashboard.php';</script>";
     } else {
         print "<script>alert('Usuário ou senha incorreta');</script>";
         print "<script>location.href='index.php';</script>";
     }
+} else {
+    print "<script>alert('Usuário ou senha incorreta');</script>";
+    print "<script>location.href='index.php';</script>";
+}
 ?>
