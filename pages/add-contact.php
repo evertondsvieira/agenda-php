f891a5f3aea3de9c0e0910976d924cbfb838d362
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-22 21:47:03 -0300

    Fix: Fixed html body

--- pages/add-contact.php
+++ pages/add-contact.php
@@ -1,25 +1,38 @@
-<div class="centralize-form">
-    <h1 class="add-name">Novo contato</h1>
-    <form action="?page=save" method="POST" id="form-test">
-        <input type="hidden" name="action" value="create">
-        <div class="centralize-input">
-            <label>Nome:</label>
-            <input class="input" type="text" name="name">
-            <p id="erro-name" style="color: red"></p>
-        </div>
-        <div class="centralize-input">
-            <label>Email:</label>
-            <input class="input" type="email" name="email">
-            <p id="erro-email" style="color: red"></p>
-        </div>
-        <div class="centralize-input">
-            <label>Telefone:</label>
-            <input class="input" type="text" name="phone">
-            <p id="erro-phone" style="color: red"></p>
-        </div>
-        <input type="hidden" name="user_id" value="<?php echo $_SESSION['id']; ?>">
-        <div class="centralize-button">
-            <button class="button-submit" type="submit" name="submit">Enviar</button>
-        </div>
-    </form>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YourAgenda</title>
+</head>
+<body>
+    <div class="centralize-form">
+        <h1 class="add-name">Novo contato</h1>
+        <form action="?page=save" method="POST">
+            <input type="hidden" name="action" value="create">
+            <div class="centralize-input">
+                <label>Nome:</label>
+                <input class="input" type="text" name="name">
+            </div>
+            <div class="centralize-input">
+                <label>Email:</label>
+                <input class="input" type="email" name="email">
+            </div>
+            <div class="centralize-input">
+                <label>Telefone:</label>
+                <input class="input" type="text" name="phone">
+            </div>
+            <input type="hidden" name="user_id" value="<?php echo $_SESSION['id']; ?>">
+            
+            <span id="erro-geral" style="color: red; font-size: 14px; font-family: Lexend;"></span>
+            
+            <div class="centralize-button">
+                <button class="button-submit" type="submit" name="submit">Enviar</button>
+            </div>
+        </form>
+    </div>
+
+    <script src="./validations/contact-validation.js"></script>
+</body>
+</html>
